c4aa3d2d769fc21da0398e088ddfb9be308f69a2
nsh <nsh2398@naver.com>
2024-08-28 21:35:09 +0900

    feat: Memento Pattern 구현 > Main 내용을 Test 메소드로 이동

--- MementoPattern/test/domain/PlayerTest.java
+++ MementoPattern/test/domain/PlayerTest.java
@@ -18,6 +18,56 @@ class PlayerTest {
     }
 
     @Test
+    void test() {
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(100);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(1000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(2000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(3000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(4000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(5000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+
+        player.adjustExp(9000);
+        System.out.println(player.printPlayerLevel());
+        player.action(5);
+
+        System.out.println("========================");
+    }
+
+    @Test
     void IllegalExp() {
         // given
 
