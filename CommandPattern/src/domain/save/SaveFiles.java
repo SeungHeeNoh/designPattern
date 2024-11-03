e5214dfb7db57d4474f21cd162a89dfefcd2096d
nsh <nsh2398@naver.com>
2024-11-03 22:27:52 +0900

    feat: Command Pattern 구현 > SaveFiles 객체 또한 저장하기 위해 멤버변수의 static 키워드 제거 및 SaveFilesSerializer class 선언

--- CommandPattern/src/domain/save/SaveFiles.java
+++ CommandPattern/src/domain/save/SaveFiles.java
@@ -1,11 +1,18 @@
 package domain.save;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import domain.Player;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Stack;
 
 public class SaveFiles {
-    private final static Stack<SaveFile> saveFiles = new Stack<>();
+    @JsonSerialize(using = SaveFilesSerializer.class)
+    private final Stack<SaveFile> saveFiles = new Stack<>();
 
     public void createSaveFile(Player player) {
         SaveFile save = player.getPlayerSaveFile();
@@ -19,6 +26,17 @@ public class SaveFiles {
         } else {
             System.out.println("save file is not exist.");
         }
+    }
 
+    public static class SaveFilesSerializer extends JsonSerializer<Stack<SaveFile>> {
+
+        @Override
+        public void serialize(Stack<SaveFile> saveFiles, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeStartArray();
+            for (SaveFile saveFile : saveFiles) {
+                jsonGenerator.writeObject(saveFile);
+            }
+            jsonGenerator.writeEndArray();
+        }
     }
 }
